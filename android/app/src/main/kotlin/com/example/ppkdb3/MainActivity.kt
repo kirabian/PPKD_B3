35c8f4124906e8c04fc5512b0e1fb64ab077106f
kirabian <fabiansyahalghiffarireal@gmail.com>
2025-07-31 11:44:38 +0700

    Upload Flutter biodata project

---
+++ android/app/src/main/kotlin/com/example/ppkdb3/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.ppkdb3
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/start.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+
+class StartWidget extends StatelessWidget {
+  const StartWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'PROFILE SAYA',
+          style: TextStyle(
+            fontSize: 24,
+            fontWeight: FontWeight.bold,
+            fontFamily: 'Bunge',
+          ),
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.amber,
+        // centerTitle: true,
+        actions: [Icon(Icons.settings)],
+        actionsPadding: const EdgeInsets.all(8.0),
+      ),
+      // floatingActionButton: FloatingActionButton(
+      //   onPressed: () {
+      //     // Action to perform when the button is pressed
+      //     ScaffoldMessenger.of(context).showSnackBar(
+      //       SnackBar(content: Text('Floating Action Button Pressed')),
+      //     );
+      //   },
+      //   child: Icon(Icons.add),
+      // ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            'Nama : Fabian Syah Al Ghiffari',
+            style: TextStyle(
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+              fontFamily: 'Bitcount',
+            ),
+          ),
+          Row(
+            children: [
+              Icon(Icons.location_on),
+              SizedBox(width: 8.0),
+              Text(
+                'Alamat : Jakarta',
+                style: TextStyle(
+                  fontSize: 22,
+                  color: const Color.fromARGB(255, 0, 0, 0),
+                  fontFamily: 'serif',
+                ),
+              ),
+            ],
+          ),
+          Column(
+            children: [
+              Text(
+                "Passionate web developer with a focus on creating dynamic and responsive web applications. Skilled in HTML, CSS, JavaScript, and various frameworks. Committed to delivering high-quality code and enhancing user experiences.",
+                style: TextStyle(
+                  fontSize: 18,
+                  color: const Color.fromARGB(255, 136, 136, 136),
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
