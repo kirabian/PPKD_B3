58c085429d183f4ef496bb46b085e51aabfd6555
kirabian <fabiansyahalghiffarireal@gmail.com>
2025-08-04 14:01:20 +0700

    Update tugas 4

--- lib/tugas_4/kopi_kenangan_clone.dart
+++ lib/tugas_4/kopi_kenangan_clone.dart
@@ -5,122 +5,97 @@ class KopiKenanganClone extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final List<Map<String, dynamic>> coffeeList = [
+      {
+        'name': 'Magic Spanish Latte',
+        'price': 'Rp 30.000',
+        'image': 'assets/images/coffees/kopi_satu.jpg',
+      },
+      {
+        'name': 'OATSIDE Magic Matcha Latte',
+        'price': 'Rp 25.000',
+        'image': 'assets/images/coffees/kopi_dua.jpg',
+      },
+      {
+        'name': 'OAATSIDE Magic Aren Latte',
+        'price': 'Rp 35.000',
+        'image': 'assets/images/coffees/kopi_tiga.jpg',
+      },
+      {
+        'name': 'O`Boy Coffee Toast',
+        'price': 'Rp 45.000',
+        'image': 'assets/images/coffees/roti_satu.jpg',
+      },
+      {
+        'name': 'Berry Love Cromboloni',
+        'price': 'Rp 20.000',
+        'image': 'assets/images/coffees/kue_satu.jpg',
+      },
+    ];
+
     return Scaffold(
       appBar: AppBar(title: Text('Kopi Kenangan Clone')),
       body: ListView(
+        padding: EdgeInsets.all(16),
         children: [
-          SingleChildScrollView(
-            child: Stack(
-              children: [
-                Container(
-                  padding: EdgeInsets.all(16),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        'Formulir Pengguna:',
-                        style: TextStyle(
-                          fontSize: 18,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      SizedBox(height: 16),
-                      TextField(
-                        decoration: InputDecoration(
-                          labelText: 'Nama',
-                          border: OutlineInputBorder(),
-                          prefixIcon: Icon(Icons.person),
-                        ),
-                      ),
-                      SizedBox(height: 12),
-                      TextField(
-                        keyboardType: TextInputType.emailAddress,
-                        decoration: InputDecoration(
-                          labelText: 'Email',
-                          border: OutlineInputBorder(),
-                          prefixIcon: Icon(Icons.email),
-                        ),
-                      ),
-                      SizedBox(height: 12),
-                      TextField(
-                        decoration: InputDecoration(
-                          labelText: 'No. HP',
-                          border: OutlineInputBorder(),
-                          prefixIcon: Icon(Icons.phone),
-                        ),
-                        keyboardType: TextInputType.phone,
-                      ),
-                      SizedBox(height: 12),
-                      TextField(
-                        decoration: InputDecoration(
-                          labelText: 'Deskripsi',
-                          border: OutlineInputBorder(),
-                          prefixIcon: Icon(Icons.description),
-                        ),
-                        maxLines: 3,
-                      ),
-                      SizedBox(height: 16),
-                      ListTile(
-                        leading: Image.asset(
-                          'assets/images/coffees/kopi_satu.jpg',
-                          width: 40,
-                          height: 40,
-                          fit: BoxFit.cover,
-                        ),
-                        title: Text('Magic Spanish Latte'),
-                        subtitle: Text('Rp 30.000'),
-                        trailing: Icon(Icons.add_shopping_cart),
-                      ),
-                      ListTile(
-                        leading: Image.asset(
-                          'assets/images/coffees/kopi_dua.jpg',
-                          width: 40,
-                          height: 40,
-                          fit: BoxFit.cover,
-                        ),
-                        title: Text('OATSIDE Magic Matcha Latte'),
-                        subtitle: Text('Rp 25.000'),
-                        trailing: Icon(Icons.add_shopping_cart),
-                      ),
-                      ListTile(
-                        leading: Image.asset(
-                          'assets/images/coffees/kopi_tiga.jpg',
-                          width: 40,
-                          height: 40,
-                          fit: BoxFit.cover,
-                        ),
-                        title: Text('OAATSIDE Magic Aren Latte'),
-                        subtitle: Text('Rp 35.000'),
-                        trailing: Icon(Icons.add_shopping_cart),
-                      ),
-                      ListTile(
-                        leading: Image.asset(
-                          'assets/images/coffees/roti_satu.jpg',
-                          width: 40,
-                          height: 40,
-                          fit: BoxFit.cover,
-                        ),
-                        title: Text('O`Boy Coffee Toast'),
-                        subtitle: Text('Rp 45.000'),
-                        trailing: Icon(Icons.add_shopping_cart),
-                      ),
-                      ListTile(
-                        leading: Image.asset(
-                          'assets/images/coffees/kue_satu.jpg',
-                          width: 40,
-                          height: 40,
-                          fit: BoxFit.cover,
-                        ),
-                        title: Text('Berry Love Cromboloni'),
-                        subtitle: Text('Rp 20.000'),
-                        trailing: Icon(Icons.add_shopping_cart),
-                      ),
-                    ],
-                  ),
-                ),
-              ],
+          Text(
+            'Formulir Pengguna:',
+            style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+          ),
+          SizedBox(height: 16),
+          TextField(
+            decoration: InputDecoration(
+              labelText: 'Nama',
+              border: OutlineInputBorder(),
+              prefixIcon: Icon(Icons.person),
             ),
           ),
+          SizedBox(height: 12),
+          TextField(
+            keyboardType: TextInputType.emailAddress,
+            decoration: InputDecoration(
+              labelText: 'Email',
+              border: OutlineInputBorder(),
+              prefixIcon: Icon(Icons.email),
+            ),
+          ),
+          SizedBox(height: 12),
+          TextField(
+            decoration: InputDecoration(
+              labelText: 'No. HP',
+              border: OutlineInputBorder(),
+              prefixIcon: Icon(Icons.phone),
+            ),
+            keyboardType: TextInputType.phone,
+          ),
+          SizedBox(height: 12),
+          TextField(
+            decoration: InputDecoration(
+              labelText: 'Deskripsi',
+              border: OutlineInputBorder(),
+              prefixIcon: Icon(Icons.description),
+            ),
+            maxLines: 3,
+          ),
+          SizedBox(height: 24),
+          Text(
+            'Daftar Produk:',
+            style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+          ),
+          SizedBox(height: 12),
+          ...coffeeList.map((coffee) {
+            return ListTile(
+              leading: Image.asset(
+                coffee['image'],
+                width: 40,
+                height: 40,
+                fit: BoxFit.cover,
+              ),
+              title: Text(coffee['name']),
+              subtitle: Text(coffee['price']),
+              trailing: Icon(Icons.add_shopping_cart),
+            );
+          }).toList(),
         ],
       ),
     );
