31acebf77fe53bd9bb86f0f869633d716620f7e5
kirabian <fabiansyahalghiffarireal@gmail.com>
2025-08-27 07:29:04 +0700

    on progress

--- lib/tugas11/views/rick_api/views/character_screen.dart
+++ lib/tugas11/views/rick_api/views/character_screen.dart
@@ -2,8 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:ppkdb3/tugas11/model/character_model.dart';
 import 'package:ppkdb3/tugas11/views/rick_api/get_api.dart';
 import 'package:ppkdb3/tugas11/views/rick_api/views/character_detail.dart';
-// import 'package:ppkd_b_3/day_23/api/get_user.dart';
-// import 'package:ppkd_b_3/day_23/model/user_model.dart';
 
 class RickApi extends StatefulWidget {
   const RickApi({super.key});
@@ -14,66 +12,39 @@ class RickApi extends StatefulWidget {
 }
 
 class _RickApiState extends State<RickApi> {
-  late Character character;
-  @override
-  // void initState() {
-  //   super.initState();
-  //   getUser();
-  // }
-  @override
-  void handleTapOnCard() {
-    Navigator.push(
-      context,
-      MaterialPageRoute(
-        builder: (context) => CharacterDetail(character: character),
-      ),
-    );
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SingleChildScrollView(
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              FutureBuilder<List<Character>>(
-                future: getCharacter(),
-                builder: (BuildContext context, AsyncSnapshot snapshot) {
-                  if (snapshot.connectionState == ConnectionState.waiting) {
-                    return CircularProgressIndicator();
-                  } else if (snapshot.hasData) {
-                    final users = snapshot.data as List<Character>;
-                    return GestureDetector(
-                      onTap: handleTapOnCard,
-                      child: ListView.builder(
-                        physics: NeverScrollableScrollPhysics(),
-                        shrinkWrap: true,
-                        itemCount: users.length,
-                        itemBuilder: (BuildContext context, int index) {
-                          final dataChar = users[index];
-                          return Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: ListTile(
-                              leading: Image.network(dataChar.image ?? ""),
-                              title: Text(dataChar.name ?? ""),
-                              // subtitle: Text(
-                              //   "${dataChar.firstName} ${dataChar.lastName}" ?? "",
-                              // ),
-                            ),
-                          );
-                        },
+      body: FutureBuilder<List<Character>>(
+        future: getCharacter(),
+        builder: (BuildContext context, AsyncSnapshot snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          } else if (snapshot.hasData) {
+            final users = snapshot.data as List<Character>;
+            return ListView.builder(
+              itemCount: users.length,
+              itemBuilder: (BuildContext context, int index) {
+                final dataChar = users[index];
+                return ListTile(
+                  leading: Image.network(dataChar.image ?? ""),
+                  title: Text(dataChar.name ?? ""),
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) =>
+                            CharacterDetail(character: dataChar),
                       ),
                     );
-                  } else {
-                    return Text("Gagal Memuat data");
-                  }
-                },
-              ),
-            ],
-          ),
-        ),
+                  },
+                );
+              },
+            );
+          } else {
+            return const Center(child: Text("Gagal Memuat data"));
+          }
+        },
       ),
     );
   }
